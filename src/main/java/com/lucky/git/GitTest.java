1228ad76a1940c5ce8df53ed825c2ecb7c2bbddb
LuckyE <llb1120@outlook.com>
2022-09-08 16:35:58 +0800

    push-test commit

--- src/main/java/com/lucky/git/GitTest.java
+++ src/main/java/com/lucky/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("Hello git5!");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
